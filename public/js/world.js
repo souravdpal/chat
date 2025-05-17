073dbc7db95d3135c0bfc682cf274f4f3ab10665
souravdpal <sourav2026resolution@gmail.com>
2025-05-17 16:26:37 +0530

    adding freind request

--- public/js/world.js
+++ public/js/world.js
@@ -6,8 +6,46 @@ const form = document.getElementById('btn');
 const input = document.getElementById('take');
 const name1 = localStorage.getItem('name') // fallback
 let joiner = document.getElementById("join");
-joiner.innerHTML += `${name1} joined the chat!`;
+joiner.innerHTML += `${name1} joined the world chat!`;
 
+fetch('/f' ,{
+  method :'POST'
+})
+.then(response=>response.json())
+.then(data=>{
+  let from  = data.from
+  if(data.to == name1 ){
+    let ask = prompt(`${from} send you freind req enter y/n`)
+    console.log(ask)
+    if(ask == 'y' || 'Y'){
+      let ans1= "ok"
+      fetch('/f_res', {
+        method : "POST",
+        headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ ans : ans1})
+      })
+    }else if (ask  === 'n' && 'N'){
+      let ans1 = "denied"
+      fetch('/f_res', {
+        method : "POST",
+        headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ ans : ans1})
+      })
+  
+  
+  
+  
+    }
+  }else {
+    console.log('no this is not that user!')
+
+  }
+  
+})
 // Send message
 form.addEventListener('click', (e) => {
   e.preventDefault();
